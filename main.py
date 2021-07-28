83f3044a12da225f9db417f6dcf2b85b4849ba2f
Dorin07 <47871786+Dorin07@users.noreply.github.com>
2021-07-29 00:41:39 +0300

    fixed all the libraries, remains to test the code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import cv2 as cv
+import cv2
 import numpy as np
 from scipy.spatial import distance as dist
 from imutils import face_utils
@@ -9,14 +9,99 @@ import imutils
 import time
 import dlib
 
-cameraID = 0
+"""
+TO DO: fix the shape detector
+"""
 
-#creating camera object
-camera = cv.VideoCapture(cameraID)
+def eye_aspect_ratio(eye):
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    a = dist.euclidean(eye[1], eye[5])
+    b = dist.euclidean(eye[2], eye[4])
+
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    c = dist.euclidean(eye[0], eye[3])
+
+    # compute the eye aspect ratio
+    ear = (a + b) / (2.0 * c)
+
+    # return the eye aspect ratio
+    return ear
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+                help="path to facial landmark predictor")
+ap.add_argument("-v", "--video", type=str, default="",
+                help="path to input video file")
+args = vars(ap.parse_args())
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 3
+
+counter = 0
+total = 0
+
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left-eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right-eye"]
+
+print("[INFO] starting video stream thread...")
+# vs = FileVideoStream(args["video"]).start()
+# fileStream = True
+vs = VideoStream(src=0).start()
+# vs = VideoStream(usePiCamera=True).start()
+fileStream = False
+
+time.sleep(1.0)
 
 while True:
-    #getting frame from camera
-    ret, frame = camera.read()
+    if fileStream and not vs.more():
+        break
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #showing the frame on the screen
-    cv.imshow('Frame', frame)
+    rects = detector(gray, 0)
+
+    for rect in rects:
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+
+        ear = (leftEAR + rightEAR) / 2.0
+
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        if ear < EYE_AR_THRESH:
+            counter += 1
+        else:
+            if counter >= EYE_AR_CONSEC_FRAMES:
+                total += 1
+            counter = 0
+
+        cv2.putText(frame, "Blinks: {}".format(total), (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "Ear: {:.2f}".format(ear), (300, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
